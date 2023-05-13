d90c8ec0f76ade9b31620773a8fcc7f17427bf95
Crystal0827 <119652511+Crystal0827@users.noreply.github.com>
2023-05-13 22:34:43 +0800

    Update a.cpp

--- a.cpp
+++ a.cpp
@@ -7,7 +7,8 @@ int main ()
      string pattern_jpg; 
      vector<cv::String> img_files;
       pattern_jpg = "/media/crystal/Data/Test_project/Example/*.jpg"; // “*” 代指这类图片的名称，其后跟图片格式。
-     cv::glob(pattern_jpg,img_files); //glob()函数是用来收集一个文件夹中某类图片的，第一个参数是将图片地址及格式输入一个string变量中；第二个参数是结果，即你所需要的图片。
+     cv::glob(pattern_jpg,img_files); //  
+   glob()函数是用来收集一个文件夹中某类图片的，第一个参数是将图片地址及格式输入一个string变量中；第二个参数是结果，即你所需要的图片。用此函数后只需按一次F5和多次任意键即可识别全部图片。
      for(int i = 0;i<img_files.size();i++)
      {
       if(img_files[i].empty())
